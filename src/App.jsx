b4eec596bf470d6336647a3a8650a0a06381e8d4
nwachukwuchukwuka <nwachukwuadinoyi@gmail.com>
2025-03-08 21:04:47 +0100

    capitalized A in app

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import Navbar from './ui_components/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './ui_components/HomePage'
 import About from './pages/About'
-import "./app.css"
 import Footer from './ui_components/Footer'
 import Services from './pages/Services'
 import ContactUs from './pages/ContactUs'
 import ScrollToTop from './ScrollToTop'
+import "./App.css"
 const App = () => {
   return (
     <div>
